dbe3fd864bcc4ebcce1f1d3f7bf58f8ef8d6440f
soyfeo123 <pabloivancamarena@gmail.com>
2023-08-09 19:17:16 -0700

    ,,,

--- calming-client-server/autoplay_app.js
+++ calming-client-server/autoplay_app.js
@@ -1,12 +1,10 @@
 // JavaScript source code
-function onCLIck() {
-    var hasBeenFound = false;
-    while (hasBeenFound == false) {
-        setTimeout(function () {
-            if (document.querySelector(".btn-common-play-game-lg")) {
-                hasBeenFound = true;
-            }
-        }, 500)
-    };
-    document.querySelector(".btn-common-play-game-lg").click();
+function attemptClick() {
+    const playButton = document.querySelector(".btn-common-play-game-lg");
+    if (playButton) {
+        document.querySelector(".btn-common-play-game-lg").click();
+    }
 };
+function onCLIck() {
+    attemptClick();
+}
